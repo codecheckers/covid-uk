435a22770f70477c733c818055e3b57ddeafa0e5
Stephen J. Eglen <sje30@cam.ac.uk>
2020-04-02 23:54:02 +0100

    Record of what needs installing (R and system packages)

---
+++ install.R
@@ -0,0 +1,18 @@
+## Add a script for installing required packages
+
+install.packages(c("rlang", "stringr", "qs"))
+install.packages(c("ggplot2", "Rcpp", "lubridate", "nloptr", "HDInterval"))
+
+## To install the following package, we also need GSL to be installed
+## within linux.
+## On arch I did this using:
+## sudo pacman -S gsl
+## Does this mean that I will need a Dockerfile rather than just
+## use the binder text files to set up R?
+install.packages("RcppGSL")
+
+## Also, nlopt package is required.
+## yaourt -S nlopt
+
+
+
