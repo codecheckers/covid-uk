4f4694a5fa8e64b038ca0826b43a8b5621e3dc91
Stephen J. Eglen <sje30@cam.ac.uk>
2020-04-12 10:48:07 +0100

    Tidy up and comment what has worked in zenodo.

--- codecheck/codecheck-zenodo.R
+++ codecheck/codecheck-zenodo.R
@@ -3,7 +3,11 @@
 ## Code taken from Daniel's Hopfield reproduction.
 
 require(zen4R)
+require(yaml)
+yaml_file = "../codecheck.yml"
+codecheck_yaml = read_yaml( yaml_file) 
 
+## The following line ma work only for SJE
 my_token = system("pass show codechecker-token", intern=TRUE)
 
 zenodo <- ZenodoManager$new(
@@ -11,7 +15,7 @@ zenodo <- ZenodoManager$new(
    logger = "INFO"
 )
 
-myrec <- zenodo$createEmptyRecord()
+##myrec <- zenodo$createEmptyRecord()
 
 
 ## 10.5281/zenodo.3746024
@@ -22,11 +26,13 @@ deposit_draft <- zenodo$getDepositionByConceptDOI("10.5281/zenodo.3746024")
 # add metadata
 deposit_draft$setPublicationType("report")
 deposit_draft$setCommunities(communities = c("codecheck"))
-##deposit_draft$setTitle(paste("CODECHECK certificate", codecheck_yaml$certificate))
-deposit_draft$setTitle(paste("CODECHECK certificate", "xyz"))
+deposit_draft$setTitle(paste("CODECHECK certificate", codecheck_yaml$certificate))
+# sje -- yes, we should do this, for now I just added my orcid.
 # could also add codechecker as author
 # deposit_draft$addContributor(), but need to split firstname/lastname, handle optiona ORCID etc.
 deposit_draft$setLanguage(language = "eng")
+deposit_draft$setLanguage(language = "eng")
+deposit_draft$setDescription(paste("CODECHECK certificate for paper:", codecheck_yaml$paper$title))
 deposit_draft$setKeywords(keywords = c("CODECHECK"))
 deposit_draft$setNotes(notes = c("See file LICENSE for license of the contained code. The report document codecheck.pdf is published under CC-BY 4.0 International."))
 deposit_draft$setAccessRight(accessRight = "open")
@@ -34,9 +40,14 @@ deposit_draft$setLicense(licenseId = "other-open")
 deposit_draft$addRelatedIdentifier(relation = "isSupplementTo", identifier = codecheck_yaml$repository)
 deposit_draft$addRelatedIdentifier(relation = "isSupplementTo", identifier = codecheck_yaml$paper$reference)
 
+
 deposit_draft <- zenodo$depositRecord(deposit_draft)
 
-                  ## Created an zenodo token with permission to do everything using:
+## Upload the file itself when ready.
+zenodo$uploadFile("codecheck.pdf", deposit_draft$id)
+
+
+## Created an zenodo token with permission to do everything using:
 ## https://zenodo.org/account/settings/applications/tokens/new/
 ## and then stored in my password manager, using
 ## pass insert codechecker-token
